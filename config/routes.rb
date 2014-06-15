24fa712542d15d3f9b969d3ff1252e998edfa651
Phil Lewis <phglewis@yahoo.com>
2014-06-15 18:29:19 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/contact'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
